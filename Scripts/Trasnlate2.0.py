10d2f2a15f01b9ef9b0c9e18bfaeff83238f49f9
Matheus <matzallima@hotmail.com>
2020-01-09 17:25:32 -0300

    Comentarios

--- Scripts/Trasnlate2.0.py
+++ Scripts/Trasnlate2.0.py
@@ -1,24 +1,24 @@
 import tkinter as tk
 from googletrans import Translator
 
-win = tk.Tk()
-win.title("Tradutor")
-win.geometry("400x100")
+win = tk.Tk() ## criaçao da tela
+win.title("Tradutor") ## titulo da janela
+win.geometry("400x100") ## tamanho da tela
 
-def translation():
-    word = entry.get()
-    translator = Translator(service_urls=['translate.google.com'])
-    translation1 = translator.translate(word, dest="pt")
-    label1 = tk.Label(win, text=f"Traduçao: {translation1.text}", bg="yellow")
+def tadutor():
+    palavra = entry.get()
+    translator = Translator(service_urls=['translate.google.com']) ## conecçao com o site tradutor 
+    translation1 = translator.translate(palavra, dest="pt") ## escohe a linguagem pra qual sera traduzida
+    label1 = tk.Label(win, text=f"Traduçao: {translation1.text}", bg="red")
     label1.grid(row=2, column=0)
 
-label = tk.Label(win, text="Enter Word: ")
-label.grid(row=0, column=0, sticky="W")
+janela = tk.Label(win, text="Palavra ou texto: ")
+janela.grid(row=0, column=0, sticky="W")
 
 entry = tk.Entry(win)
 entry.grid(row=1, column=0)
 
-button = tk.Button(win, text="Traslate", command = translation)
-button.grid(row=1, column=2)
+botao = tk.Button(win, text="Traduzir", command = tadutor)
+botao.grid(row=1, column=2)
 
 win.mainloop()
