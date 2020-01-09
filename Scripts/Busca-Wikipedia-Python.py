bbf34a5aceda71ef8a4eb8fd3c359111296a9db9
Matheus <matzallima@hotmail.com>
2020-01-09 18:36:44 -0300

    Script bara busca na wikipedia

---
+++ Scripts/Busca-Wikipedia-Python.py
@@ -0,0 +1,24 @@
+import wikipedia
+from tkinter import *
+from tkinter.messagebox import showinfo
+
+
+win = Tk() ## criaçao da tela
+win.title("Buscar") ## titulo da janela
+win.geometry("400x100") ## tamanho da tela
+
+def pesquisar():
+    pesquisa = entry.get()
+    pergunta = wikipedia.summary(pesquisa)
+    showinfo("Resultado: ",pergunta)
+
+janela = Label(win, text="Busca: ")
+janela.grid(row=0, column=0)
+
+entry =Entry(win)
+entry.grid(row=0, column=1) #mudar
+
+botao = Button(win, text="Pesquisar", command = pesquisar)
+botao.grid(row=2, column=1)
+
+win.mainloop()
